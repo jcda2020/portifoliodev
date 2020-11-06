9cf71bbe71c76ab57efc00d5160e1cc9e3b1157b
jcda2020 <68362593+jcda2020@users.noreply.github.com>
2020-11-06 12:02:43 -0300

    Create arrays.js

---
+++ ProjetoBasicoJavaScript/arrays.js
@@ -0,0 +1,140 @@
+
+
+
+
+const notas = [7.5, 6.5, 8.5, 1.5]
+notas.push({id:3}, true, null, 'DAM')
+
+console.log(typeof notas)
+
+//console.log(notas.length)
+// acesso a índice inexistente
+//notas.push({id:3}, true, null, 'DAM') coloca no final 
+//notas.pop() remove o último elemento
+// notas.shift() remove o primeiro
+//delete valores [4]
+// console.log(typeof notas)
+// notas.unshift () adiciona na primeira prosição 
+// splice pode adicionar ou remover elementos.
+
+//notas.splice (posicao, "elementos") adicona
+// notas.splice(posicao, quantidade) remove
+
+//slice retorna um novo array a partir de uma parte do array
+
+//const turma = notas.slice(2) -> neste caso a partir do índice 2.
+
+// const turma2 = notas.slice (1,4) do primeiro até antes do último indicado
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
