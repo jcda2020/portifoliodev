5dd2df6a38adcc633e37137d1c12b7754a4e532f
jcda2020 <68362593+jcda2020@users.noreply.github.com>
2020-11-06 12:03:45 -0300

    Create json.js

---
+++ ProjetoBasicoJavaScript/json.js
@@ -0,0 +1,5 @@
+const primos = {x:1, y:3, w:5, p:"primos", 
+soma(){return x + y + w}}
+//console.log(JSON.stringify(primos))                
+
+console.log(JSON.parse('{"x":1,"y":3,"w":5,"p":"primos"}'))
