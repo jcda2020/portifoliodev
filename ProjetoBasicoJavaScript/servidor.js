da48577751193c1dfefc6d3de3f987ec641c666e
jcda2020 <68362593+jcda2020@users.noreply.github.com>
2020-11-06 12:01:30 -0300

    Create servidor.js

---
+++ ProjetoBasicoJavaScript/servidor.js
@@ -0,0 +1,16 @@
+const http = require ("http")
+const host = "127.0.0.1"
+const port = 3000
+
+const server = http.createServer((req, res)=> {
+    res.statusCode = 200;
+    res.setHeader('Content-type','text/plain');
+    res.end ("Tudo certo \n modulo funcionando");
+
+
+
+})
+
+server.listen(port, host,()=> {
+
+console.log(`Servidor rodando em http://${host}:${port}`)})
