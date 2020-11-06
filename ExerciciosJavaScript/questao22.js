d1a2fb6bfbca0d4a7ae7105c22478cdda48f045b
jcda2020 <68362593+jcda2020@users.noreply.github.com>
2020-11-06 12:02:20 -0300

    Create questao22.js

---
+++ ExerciciosJavaScript/questao22.js
@@ -0,0 +1,28 @@
+
+// demonstrar o sistema de módulos 
+function anuidade(mes, valor) {
+
+let valorAnuidade
+let i = 0
+if (mes > 1 )
+    i = 0.05
+//valorAnuidade = valor * Math.pow((1 + i),mes)
+valorAnuidade = valor * (1 + i)**mes
+ //valorAnuidade = jurosCompostos(valor,i,mes)
+return valorAnuidade
+}
+
+const a = (m,v) => {
+let i = 0  
+if (m > 1)
+i= 0.05
+
+valor = v * (1 + i)**m
+
+return valor
+
+
+}
+
+console.log(anuidade(3, 100))
+console.log(a(3,100))
